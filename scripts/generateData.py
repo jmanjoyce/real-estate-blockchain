804910eb0905ddff425f62b6878fbb09f6c34a3c
Abby Mueller <68559641+amueller8@users.noreply.github.com>
2023-12-13 15:52:17 -0500

    write json data to file s

--- scripts/generateData.py
+++ scripts/generateData.py
@@ -18,16 +18,20 @@ def get_random_object(lines):
 
 def create_addr_string(obj):
     new_addr = ""
-    new_addr += (obj["properties"]["number"] + " " \
-         + obj["properties"]["street"] + ", ")
-    if obj["properties"]["city"] != "":
-        new_addr += (obj["properties"]["city"] + " " \
-            + obj["properties"]["region"] + " " +  obj["properties"]["postcode"])
-    else:
-        new_addr += (obj["properties"]["district"] + " " \
-            + obj["properties"]["region"] + ", " +  obj["properties"]["postcode"])
-  
-    return new_addr
+    try:
+        new_addr += (obj["properties"]["number"] + " " \
+            + obj["properties"]["street"] + ", ")
+        if obj["properties"]["city"] != "":
+            new_addr += (obj["properties"]["city"] + " " \
+                + obj["properties"]["region"] + " " +  obj["properties"]["postcode"])
+        else:
+            new_addr += (obj["properties"]["district"] + " " \
+                + obj["properties"]["region"] + ", " +  obj["properties"]["postcode"])
+    
+        return new_addr
+    except TypeError as e:
+        print(e)
+        return ""
 
 def generate_test_sets():
 
@@ -38,22 +42,34 @@ def generate_test_sets():
     #choose a random address
     ranges = [1000, 10000, 100000]
     range_to_set = [tuple([1000, json_1k]), tuple([10000, json_10k]), tuple([100000, json_100k])]
+    range_to_filename = [tuple([1000, "scripts/assets/json_1k.json"]), tuple([10000, "scripts/assets/json_10k.json"]), tuple([100000, "scripts/assets/json_100k.json"])]
+    for r, f in range_to_filename:
+        with open(f, 'w') as file:
+            print("Reset.")
 
     with open("scripts/assets/source.geojson", 'r') as file:
         lines = file.readlines()
 
-    for r, s in range_to_set:
-        while len(s) < r:
+    for r, s in range_to_filename:
+        total_added = 0
+        while total_added < r:
             #choose a random line from the source.geojson
             obj = get_random_object(lines)
             add_str = create_addr_string(obj)
+            if add_str == "":
+                continue
+                
             #reformat
             new_obj = {"name": "Abby", "address": add_str, "price": random.randint(70000, 150000)}
-            s.add(json.dumps(new_obj))
+            #s.add(json.dumps(new_obj))
+            with open(s, 'a') as file:
+                json.dump(new_obj, file)
+                file.write('\n')
+                total_added += 1
         
-        #print(s)
+        print("Finished with file " + s + ".\n")
     
-    return [json_1k, json_10k, json_100k]
+    #return [json_1k, json_10k, json_100k]
 
 sets = generate_test_sets()
 print(sets[0])
